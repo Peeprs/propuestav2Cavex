aa77b5ede3536bf289ee1fb434c0a0c0c59a6969
Peeprs <axelcrewsegundocrew@gmai.com>
2026-05-14 14:19:41 -0600

    Migración de ASP.NET MVC a HTML, CSS y JS puro

--- propuestav2Cavex/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace propuestav2Cavex.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
